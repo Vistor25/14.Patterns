2df032d20bb1d2680c8a4f12b6844dd08ab5a0d0
Vistor25 <victorvoronko@gmail.com>
2018-01-24 15:38:31 +0300

    add

---
+++ Builder/Builder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class ConnectionStringBuilder
+    {
+        public string DataSource { get; set; }
+        public string InitialCatalog { get; set; }
+        public bool Encrypt { get; set; }
+        public string UserID { get; set; }
+        public  string Password { get; set; }
+        public  int ConnectTimeout { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (DataSource != null)
+            {
+                sb.Append("Data Source=" + DataSource + ";");
+            }
+            if (InitialCatalog != null)
+            {
+                sb.Append("Initial Catalog=" + InitialCatalog + ";");
+            }
+
+            if (Encrypt)
+            {
+                sb.Append("Encrypt=" + Encrypt + ";");
+            }
+            if (UserID != null)
+            {
+                sb.Append("User ID=" + UserID + ";");
+            }
+            if (Password != null)
+            {
+                sb.Append("Password=" + Password + ";");
+            }
+            if (ConnectTimeout != 0)
+            {
+                sb.Append("Connect Timeout=" + ConnectTimeout + ";");
+            }
+            return sb.ToString();
+        }
+    }
+    
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Builder;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionStringBuilder connection = new ConnectionStringBuilder();
+            connection.DataSource = "MyDB";
+            connection.UserID = "sqlserver1";
+            connection.Password = "123456789";
+            connection.InitialCatalog = "server1";
+            connection.ConnectTimeout = 45;
+            Console.WriteLine(connection);
+            Console.ReadKey();
+        }
+    }
+}
